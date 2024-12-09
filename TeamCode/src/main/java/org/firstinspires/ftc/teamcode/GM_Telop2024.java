4b521ce3a559fe182cc8d98b3dd3ae4f03d6bb7d
Thomas <kittyboom78@gmail.com>
2024-12-09 18:13:42 -0500

    armRotator 2 not finished. This update is mostly updated comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GM_Telop2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GM_Telop2024.java
@@ -16,19 +16,20 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 
-@TeleOp(name="GM_Telop2024", group="GreenMachine")  //declares the name of the class and the
-// group it is in.
+@TeleOp(name="GM_Telop2024", group="GreenMachine")  // Dares the name of the class and the group it is in.
 
 public class GM_Telop2024 extends OpMode {
 
     final double ARM_TICKS_PER_DEGREE =
-            537.6 // number of encoder ticks per rotation of the bare motor
+            537.6 // Number of encoder ticks per rotation of the bare motor
                     * 19.2 / 1.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
                     * 1 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
                     * 1/360.0; // we want ticks per degree, not per rotation
 
-//28.7 IS ARM_TICKS_PER_DEGREE
+// 28.7 IS ARM_TICKS_PER_DEGREE
 
+
+    // Positions for the arms
     final double ARM_COLLAPSED_INTO_ROBOT  = 0;
     final double GROUND_POS                = 0 * ARM_TICKS_PER_DEGREE;
     final double ARM_CLEAR_BARRIER         = 15 * ARM_TICKS_PER_DEGREE;
@@ -36,30 +37,6 @@ public class GM_Telop2024 extends OpMode {
     final double LOW_BASKET                = 90 * ARM_TICKS_PER_DEGREE;
     final double HIGH_BASKET               = 110 * ARM_TICKS_PER_DEGREE;
     final double ARM_WINCH_ROBOT           = 10  * ARM_TICKS_PER_DEGREE;
-
-    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements.
-    final double INTAKE_COLLECT    = -1.0;
-    final double INTAKE_OFF        =  0.0;
-    final double INTAKE_DEPOSIT    =  0.5;
-
-    /* Variables to store the positions that the wrist should be set to when folding in, or folding out.
-    final double WRIST_FOLDED_IN   = 0.1667;
-    final double WRIST_FOLDED_OUT  = 0.5;
-
-    /* A number in degrees that the triggers can adjust the arm position by
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
-
-    /* Variables that are used to set the arm to a specific position
-    double armPosition = (int)ARM_COLLAPSED_INTO_ROBOT;
-    double armPositionFudgeFactor;
-
-    final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
-
-    final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
-    final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
-    final double LIFT_SCORING_IN_HIGH_BASKET = 480 * LIFT_TICKS_PER_MM;
-
-    double liftPosition = LIFT_COLLAPSED; */
     
     double armPosition = 0;
 
@@ -67,7 +44,7 @@ public class GM_Telop2024 extends OpMode {
     final double MAX_ARM_POS = 1050;
 
 
-    double  //declares all double variables and their values
+    double  // Declares all double variables and their values
             speedVariable = .8;
     int speedVariable1=0;
 
@@ -80,7 +57,7 @@ double liftPosition = LIFT_COLLAPSED;
 
     /*
      * Code will run ONCE when the driver hits INIT
-     * INIT means initialize
+     * INIT means initialize. If you're new to the stream Calc is short for calculator; I'm just using slang.
      */
     DcMotorEx
             rearLeft = null,
@@ -91,63 +68,22 @@ double liftPosition = LIFT_COLLAPSED;
             armRotator2 = null,
             armSlide = null;
 
-          //  liftMotor = null;
-   // servo names/declarations
+    // servo names/declarations
 Servo
     wrist = null;
+
     // Continuous rotation servo
     CRServo
     intake = null;
 
 
-    // pixelArm = null;
-    // double hangerpos = 0.0;
-
-    /* These constants hold the position that the arm is commanded to run to.
-   These are relative to where the arm was located when you start the OpMode. So make sure the
-   arm is reset to collapsed inside the robot before you start the program.
-
-   In these variables you'll see a number in degrees, multiplied by the ticks per degree of the arm.
-   This results in the number of encoder ticks the arm needs to move in order to achieve the ideal
-   set position of the arm. For example, the ARM_SCORE_SAMPLE_IN_LOW is set to
-   160 * ARM_TICKS_PER_DEGREE. This asks the arm to move 160° from the starting position.
-   If you'd like it to move further, increase that number. If you'd like it to not move
-   as far from the starting position, decrease it. */
-   /* final double ARM_COLLAPSED_INTO_ROBOT = 0;
-    final double ARM_COLLECT = 0 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER = 15 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN = 90 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW = 90 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK = 110 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT = 10 * ARM_TICKS_PER_DEGREE;*/
     HardwareMap hwMap = null;
     ElapsedTime runTime = new ElapsedTime();
-    /* A number in degrees that the triggers can adjust the arm position by */
-    // final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
-    /* Variables that are used to set the arm to a specific position */
-    // double armPosition = (int) ARM_COLLAPSED_INTO_ROBOT;
-    //double armPositionFudgeFactor;
-    // final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
-
-    //  final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
-    //  final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
-    //  final double LIFT_SCORING_IN_HIGH_BASKET = 480 * LIFT_TICKS_PER_MM;
-
-    // double liftPosition = LIFT_COLLAPSED;
-   /* double armLiftComp = 0;
-    double cycletime = 0;
-    double looptime = 0;
-    double oldtime = 0;
-    double liftpos=0;
-    double liftpower=0.7;
-    double liftposincrement=1.1;
-    double minliftposition=2;
-    double maxliftposition=500;*/
 
     @Override
     public void init() { //initialization class to be used at start of tele-op
 
-        // these are our motors and what they are called
+        // Motor Names
         rearLeft = hardwareMap.get(DcMotorEx.class, "rearLeft");
         rearRight = hardwareMap.get(DcMotorEx.class, "rearRight");
         frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
@@ -156,11 +92,12 @@ Servo
         armRotator2 = hardwareMap.get(DcMotorEx.class, "armRotator2");
         armSlide = hardwareMap.get(DcMotorEx.class, "armSlide");
         //liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
-// these are our servos and what they are called
+
+        // Servo Names
         wrist = hardwareMap.get(Servo.class, "wrist");
         intake = hardwareMap.get(CRServo.class, "intake");
 
-//Direction?
+        //Direction of motors
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         rearLeft.setDirection(DcMotor.Direction.FORWARD);
@@ -168,9 +105,8 @@ Servo
         armRotator.setDirection(DcMotor.Direction.FORWARD);
         armRotator2.setDirection(DcMotor.Direction.FORWARD);
         armSlide.setDirection(DcMotor.Direction.FORWARD);
-      //  liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        // pixelArm.setDirection(DcMotor.Direction.REVERSE);
 
+        // Sets up motors
         frontLeft.setPower(0);
         frontRight.setPower(0);
         rearLeft.setPower(0);
@@ -178,13 +114,13 @@ Servo
         armRotator.setPower(0);
         armRotator2.setPower(0);
         armSlide.setPower(0);
-       // liftMotor.setPower(0);
-
-
-// set position for servos
-        wrist.setPosition(0);
         intake.setPower(0);
 
+
+        // Sets position for servos
+        wrist.setPosition(0);
+
+        // Hi thanks for reading this comments I worked really hard on them :)
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -192,7 +128,7 @@ Servo
         armRotator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armRotator2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       // liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
 
         //Setting motors to run without encoders
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -202,10 +138,7 @@ Servo
         armRotator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         armRotator2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         armSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      //  liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        /*This sets the maximum current that the control hub will apply to the arm before throwing a flag */
-        //((DcMotorEx) armMotor).setCurrentAlert(5, CurrentUnit.AMPS);
 
         //this will send a telemetry message to signify robot waiting;
         telemetry.addLine("AUTOBOTS ROLL OUT");
@@ -223,7 +156,7 @@ Servo
     }
 
     /*
-     *this code will run ONCE when the driver hits PLAY
+     * This code will run ONCE when the driver hits PLAY
      */
     @Override
     public void start() {
@@ -240,11 +173,13 @@ Servo
     @Override
     public void loop() {
 
-        //==========================================================
-        //                        GamePad One
-        //==========================================================
 
-        //armRotator manual lifting
+        //==========================================================\\
+        //                        GamePad One                       \\
+        //==========================================================\\
+
+
+        // armRotator manual lifting mostly for testing or as a last resort
         if(gamepad1.right_trigger>.1){
         armRotator.setPower(.25);
         armRotator2.setPower(.25);
@@ -254,8 +189,9 @@ Servo
             armRotator.setPower(-.25);
             armRotator2.setPower(-.25);
         }
-        //Controls Drive Train
 
+
+        // Drive Train controls
         float FLspeed = -gamepad1.left_stick_y + gamepad1.left_stick_x;
         float BLspeed = -gamepad1.left_stick_y - gamepad1.left_stick_x;
         float FRspeed = -gamepad1.right_stick_y - gamepad1.right_stick_x;
@@ -267,18 +203,12 @@ Servo
         frontRight.setPower(Range.clip((-FRspeed * speedVariable), -1, 1));
 
 
-        //DriveTrain Speed Controls
+        // DriveTrain Speed Controls
         if (gamepad1.dpad_left) speedVariable -= 0.1;
         if (gamepad1.dpad_right) speedVariable += 0.1;
 
-        // armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         speedVariable = Range.clip(speedVariable, 0, 1);
-     /*   if (gamepad1.right_trigger > 0.5 && hangerpos < 2000)
-            hangerpos += 50;
-        if (gamepad1.left_trigger > 0.5 && hangerpos > 40)
-            hangerpos -= 40;*/
-        /*This sets the maximum current that the control hub will apply to the arm before throwing a flag */
-        // ((DcMotorEx) armMotor).setCurrentAlert(5, CurrentUnit.AMPS);
+
          /* Before starting the armMotor. We'll make sure the TargetPosition is set to 0.
         Then we'll set the RunMode to RUN_TO_POSITION. And we'll ask it to stop and reset encoder.
         If you do not have the encoder plugged into this motor, it will not run in this code. */
@@ -286,6 +216,8 @@ Servo
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setPower(0.8);
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);*/
+
+
         /* Send telemetry message to signify robot waiting */
         telemetry.addLine("Robot Ready.");
         telemetry.update();
@@ -304,30 +236,34 @@ Servo
             it folds out the wrist to make sure it is in the correct orientation to intake, and it
             turns the intake on to the COLLECT mode.*/
 
-        //==========================================================
-        //                        GamePad Two
-        //==========================================================
+        //==========================================================\\
+        //                        GamePad Two                       \\
+        //==========================================================\\
 
+        // Controls for intake
         if (gamepad2.left_bumper) {
             intake.setPower(0.5);
         }
 
-        if (gamepad2.right_bumper) {
+        else if (gamepad2.right_bumper) {
             intake.setPower(-0.5);
         }
 
-        //arm slide
+        // Controls for arm slide
         if (gamepad2.right_trigger > .1) {
             liftPosition += 350 * cycletime;
-        } else if (gamepad2.left_trigger > .1) {
+        }
+
+        else if (gamepad2.left_trigger > .1) {
             liftPosition -= 350 * cycletime;
         }
 
+        // Makes sure the lift does not go beyond parameters
         if (liftPosition > MAX_ARM_POS){
             liftPosition = MAX_ARM_POS;
         }
 
-        if (liftPosition < 0){
+        else if (liftPosition < 0){
             liftPosition = 0;
         }
 
@@ -336,21 +272,24 @@ Servo
         ((DcMotorEx) armSlide).setVelocity(200);
         armSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        // Controls for armRotator
         if (gamepad2.a) {
             armPosition = GROUND_POS;
             wrist.setPosition(.3);
         }
 
-        if (gamepad2.b) {
+        else if (gamepad2.b) {
             armPosition = LOW_BASKET;
             wrist.setPosition(.5);
         }
 
-        if (gamepad2.y) {
+        else if (gamepad2.y) {
             armPosition = HIGH_BASKET;
             wrist.setPosition(.5);
 
         }
+
+
 
         //   liftMotor.setTargetPosition((int) (liftpos));
        // liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -435,9 +374,10 @@ Servo
         cycletime = looptime-oldtime;
         oldtime = looptime;
 
-        //==========================================================//
-        //                        Telemetry                           //
-        //==========================================================//
+        //==========================================================\\
+        //                        Telemetry                         \\
+        //==========================================================\\
+
         telemetrymotorprint();
         //telemetrylift();
 
